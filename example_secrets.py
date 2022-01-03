46044ac01e08c6d7d6d9a7820902ff12d99bdc79
John Lewis <johnbchron@outlook.com>
2022-01-03 09:42:05 -0600

    added example_secrets.py

---
+++ example_secrets.py
@@ -0,0 +1,18 @@
+
+# rename to secrets.py
+
+venue_ids = {"1": "ABC", "2": "DEF", "3": "HIJ"}
+
+ccb_username = "username"
+ccb_password = "password"
+
+asana_pat = "Bearer REALLY_LONG_PERSONAL_ACCESS_TOKEN"
+
+asana_project_gid = "01234567890123456789"
+asana_test_project_gid = "01234567890123456789"
+
+asana_one_time_section_gid = "01234567890123456789"
+asana_recurring_section_gid = "01234567890123456789"
+
+asana_test_one_time_section_gid = "01234567890123456789"
+asana_test_recurring_section_gid = "01234567890123456789"
