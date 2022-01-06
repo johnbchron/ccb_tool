75b1dadb62f4e9f5eda0fe72b56aa390959a0b66
John Lewis <johnbchron@outlook.com>
2022-01-06 16:42:45 -0600

    tested emailing then returned to normal

--- run_once.py
+++ run_once.py
@@ -1,6 +1,8 @@
 
 from main import analyze_recent_events_time_diff, analyze_recent_event_two_dates
 import datetime
+from emailing import send_email
 
-# analyze_recent_events_time_diff(datetime.timedelta(hours=4), catch_errors=False)
-analyze_recent_event_two_dates(datetime.datetime(2021,6,2,6,0,0), datetime.datetime(2021,12,2,6,0,0), catch_errors=False, recurring_only=True)
+analyze_recent_events_time_diff(datetime.timedelta(hours=4), catch_errors=False)
+# analyze_recent_event_two_dates(datetime.datetime(2021,6,2,6,0,0), datetime.datetime(2021,12,2,6,0,0), catch_errors=False, recurring_only=True)
+# send_email("This is a test email. It worked")
